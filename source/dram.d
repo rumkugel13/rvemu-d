6ea7857148ba1b0481f04c6575d9ca6271259ce1
rumkugel13 <rumkugel13@outlook.com>
2023-10-10 17:16:33 +0200

    fix typo

--- source/dram.d
+++ source/dram.d
@@ -26,7 +26,7 @@ struct Dram
             case 16: return cast(ulong)dram.peek!(ushort, Endian.littleEndian)(addr - DRAM_BASE);
             case 32: return cast(ulong)dram.peek!(uint, Endian.littleEndian)(addr - DRAM_BASE);
             case 64: return dram.peek!(ulong, Endian.littleEndian)(addr - DRAM_BASE);
-            default: assert(0, "Unkwnon size " ~ size.to!string);
+            default: assert(0, "Unknown size " ~ size.to!string);
         }
     }
 
@@ -38,7 +38,7 @@ struct Dram
             case 16: dram.write!(ushort, Endian.littleEndian)(cast(ushort)value, addr - DRAM_BASE); break;
             case 32: dram.write!(uint, Endian.littleEndian)(cast(uint)value, addr - DRAM_BASE); break;
             case 64: dram.write!(ulong, Endian.littleEndian)(value, addr - DRAM_BASE); break;
-            default: assert(0, "Unkwnon size " ~ size.to!string);
+            default: assert(0, "Unknown size " ~ size.to!string);
         }
     }
 }
